63bdd5e14e51cc530e01b40ebe9a8eb0179a560a
Alisher <alisher.ibraev.2000@gmail.com>
2022-03-18 16:56:27 +0100

    db

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const mysql = require('mysql');
 
 const connectionPool = mysql.createPool({
     connectionLimit: 10,
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'alisher_2013',
     database: 'eshop',
 })
 
